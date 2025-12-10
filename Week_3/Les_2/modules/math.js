4481a1c54c6a06ed69c44d71f7eec7f2aaa24e7b
«pgm-asherblommaert» <asher.blommaert@student.arteveldehs.be>
2025-12-10 12:24:14 +0100

    Learned Modules

---
+++ Week_3/Les_2/modules/math.js
@@ -0,0 +1,12 @@
+function sum(a, b) {
+    return a + b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+module.exports = {
+    sum,
+    multiply
+};   //exporteren om later op te kunnen halen in een ander bestand
