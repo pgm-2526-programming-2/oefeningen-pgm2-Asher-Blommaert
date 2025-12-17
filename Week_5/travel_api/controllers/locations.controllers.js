29abe37381cdf09eeefc9e7d1c4c62e422f9065c
«pgm-asherblommaert» <asher.blommaert@student.arteveldehs.be>
2025-12-17 15:59:36 +0100

    Learned UID

--- Week_5/travel_api/controllers/locations.controllers.js
+++ Week_5/travel_api/controllers/locations.controllers.js
@@ -1,7 +1,9 @@
 const { getContentFromFile } = require("../utils/files.utils");
 const fileSystem = require("fs/promises");
 const path = require("path");
+const crypto = require("crypto");
 const fileLocation = path.join(__dirname, "..", "models", "locations.json")
+
 
 function getLocations(req, res)  {
     console.log("get locations");
@@ -10,7 +12,8 @@ function getLocations(req, res)  {
 
 async function addLocation(req, res) {
     const locations = await getContentFromFile("locations.json");
-    locations.push(req.body);
+    const updatedBody = { ...req.body, id: crypto.randomUUID() };
+    locations.push(updatedBody);
     await fileSystem.writeFile(fileLocation, JSON.stringify(locations, null, 4), "utf8");
     console.log("post new locations", req.body, locations);
     res.status(201);
